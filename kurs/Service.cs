597bf0c8bde968e8b1afe8a33f83b9f00eac32d6
TanyaMy <tetiana.mykhnevych@nure.ua>
2016-03-28 20:45:24 +0300

    1

---
+++ kurs/Service.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs
+{
+    class Sevice
+    {
+
+        private string name;
+        private string kind;
+        private string adress;
+        private string phone;
+        private string specialization;
+        private List<String> sevices;
+        private string ownership;
+        private DateTime startWork;
+        private DateTime endWork;
+        private List<String> workDays;
+
+
+
+
+
+    }
+}
